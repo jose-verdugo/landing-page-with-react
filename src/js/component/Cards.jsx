c57988ee719c58053493ebaf01cc91373f4868fe
José Antonio Verdugo Ardila <joseantonioverdugoardila@gmail.com>
2023-03-31 07:48:16 +0000

    mejorando la card

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Card ({title,text,btn}) {
+export default function Card ({imgURL, title,text,btn}) {
   return (
     <div className="cards d-flex justify-content-between row mx-4 my-4">
       <div className="card col-xl-3 col-lg-6 col-md-6 my-4 px-0">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={imgURL} className="card-img-top" alt="..." />
         <div className="card-body px-0">
           <h5 className="card-title">{title}</h5>
           <p className="card-text border-bottom px-4 pb-4">
@@ -16,7 +16,7 @@ export default function Card ({title,text,btn}) {
         </div>
       </div>
       <div className="card col-xl-3 col-lg-6 col-md-6 my-4 px-0">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={imgURL} className="card-img-top" alt="..." />
         <div className="card-body px-0">
           <h5 className="card-title">{title}</h5>
           <p className="card-text border-bottom px-4 pb-4">
@@ -28,7 +28,7 @@ export default function Card ({title,text,btn}) {
         </div>
       </div>
       <div className="card col-xl-3 col-lg-6 col-md-6 my-4 px-0">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={imgURL} className="card-img-top" alt="..." />
         <div className="card-body px-0">
           <h5 className="card-title">{title}</h5>
           <p className="card-text border-bottom px-4 pb-4">
@@ -40,7 +40,7 @@ export default function Card ({title,text,btn}) {
         </div>
       </div>
       <div className="card col-xl-3 col-lg-6 col-md-6 my-4 px-0">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={imgURL} className="card-img-top" alt="..." />
         <div className="card-body px-0">
           <h5 className="card-title">{title}</h5>
           <p className="card-text border-bottom px-4 pb-4">
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="Home text-center">
       <Header />
       <Jumbotron/>
-      <Card  title = "Card title" text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quam nesciunt illo, illum eveniet, iure mollitia provident molestias, natus deleniti amet! Sit distinctio quisquam laboriosam aliquam iste id, voluptates molestiae!" btn =" Go somewhere"/>
+      <Card  imgURL = "http://via.placeholder.com/500x350" title = "Card title" text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quam nesciunt illo, illum eveniet, iure mollitia provident molestias, natus deleniti amet! Sit distinctio quisquam laboriosam aliquam iste id, voluptates molestiae!" btn =" Go somewhere"/>
       <Footer />
     </div>
   );
